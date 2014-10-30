ac0edebf5dbe8968994ff09fc175cb58298ac92c
sanghoshin <sangho@onlab.us>
2014-10-29 23:47:20 -0700

    Added a test case of creating a tunnel with auto generated adjacencySID

--- TestON/tests/SRSanity/SRSanity.py
+++ TestON/tests/SRSanity/SRSanity.py
@@ -288,6 +288,169 @@ class SRSanity:
         #cleanup mininet
         main.Mininet.disconnect()
 
+
+
+#**********************************************************************************************************************************************************************************************
+# A simple test for verifying tunnels and policies with auto generated adjacencySid
+
+    def CASE5(self,main) :
+        import time
+        main.ONOS.stop()
+        # starts the controller with 3-node topology
+        main.step("Restarting ONOS and Zookeeper")
+        ret = main.ONOS.start()
+        if ret == main.FALSE:
+            main.log.report("ONOS did not start ... Aborting")
+            main.cleanup()
+            main.exit()
+        main.log.report("Running mininet")
+        main.Mininet.connect()
+        main.Mininet.handle.sendline("sudo python /home/onos/mininet/custom/testEcmp_6sw.py")
+        main.step("waiting 20 secs for switches to connect and go thru handshake")
+        time.sleep(20)
+        main.step("verifying all to all connectivity")
+
+        p1 = main.Mininet.pingHost(SRC="h2",TARGET="h1")
+        p2 = main.Mininet.pingHost(SRC="h1",TARGET="192.168.0.5")
+        pa = main.Mininet.pingall()
+        result_step1 = p1 and p2 and pa
+        utilities.assert_equals(expect=main.TRUE,actual=result_step1,
+                                  onpass="Default connectivity check PASS",
+                                  onfail="Default connectivity check FAIL")
+
+        main.step("Verifying create tunnel functionality")
+        ret = main.ONOS.create_tunnel(onosRESTIP="127.0.0.1",
+                                      onosRESTPort=str(8080),
+                                      tunnelURL="wm/onos/segmentrouting/tunnel",
+                                      params="{\"tunnel_id\":\"t2\",\"label_path\":[101,102,102002,103,103003,104,105,106]}")
+        result_step2 = ret
+        utilities.assert_equals(expect=main.TRUE,actual=result_step2,
+                                  onpass="Tunnel create check PASS",
+                                  onfail="Tunnel create check FAIL")
+
+        main.step("Verifying groups created as part tunnel t2 : 3groups@s1 and 1group@s5")
+        switch_groups = main.ONOS.get_all_groups_of_tunnel(onosRESTIP="127.0.0.1",
+                                      onosRESTPort=str(8080),
+                                      tunnelURL="wm/onos/segmentrouting/tunnel",
+                                      tunnel_id="t2")
+        print "Groups created for tunnel t1",switch_groups
+        ret_sw1 = main.FALSE
+        ret_sw5 = main.FALSE
+        ret_sw1_3groups = main.FALSE
+        ret_sw5_1groups = main.FALSE
+        ret_stats = main.FALSE
+        for entry in switch_groups:
+            if entry.has_key("SW"):
+                #print "entry[SW]: ",entry['SW']
+                if (entry['SW'] == "00:00:00:00:00:00:00:01"):
+                    ret_sw1 = main.TRUE
+                    if (len(entry['GROUPS']) == 3):
+                        ret_sw1_3groups = main.TRUE
+                if (entry['SW'] == "00:00:00:00:00:00:00:05"):
+                    ret_sw5 = main.TRUE
+                    if (len(entry['GROUPS']) == 1):
+                        ret_sw5_1groups = main.TRUE
+                before_stats = main.ONOS.get_switch_group_stats(onosRESTIP="127.0.0.1",
+                                      onosRESTPort=str(8080),
+                                      dpid=entry['SW'])
+                #print "before_stats: ",before_stats
+                if (before_stats != main.FALSE):
+                    ret_stats = main.TRUE
+                    entry['STATS'] = before_stats
+        #print "results for group check: ", ret_sw1, ret_sw1_3groups, ret_sw5, ret_sw5_1groups, ret_stats    
+        result_step3 = ret_sw1 and ret_sw1_3groups and ret_sw5 and ret_sw5_1groups and ret_stats
+        utilities.assert_equals(expect=main.TRUE,actual=result_step3,
+                                onpass="Tunnel groups check PASS",
+                                onfail="Tunnel groups check FAIL")
+
+        main.step("Verifying create policy functionality")
+        #ret = main.ONOS.create_policy("http://127.0.0.1:8080/wm/onos/segmentrouting/policy","{\"priority\": 2223, \"dst_tp_port_op\": \"eq\", \"src_tp_port_op\": \"eq\", \"src_tp_port\": \"1000\", \"tunnel_id\": \"t1\", \"src_ip\": \"10.0.1.1/32\", \"policy_type\": \"tunnel-flow\", \"dst_ip\": \"7.7.7.7/32\", \"dst_tp_port\": \"2000\", \"proto_type\": \"ip\", \"policy_id\": \"pol1\"}")
+        ret = main.ONOS.create_policy(onosRESTIP="127.0.0.1",
+                                      onosRESTPort=str(8080),
+                                      policyURL="/wm/onos/segmentrouting/policy",
+                                      params="{\"priority\": 3223, \"tunnel_id\": \"t2\", \"src_ip\": \"10.0.1.1/32\", \"policy_type\": \"tunnel-flow\", \"dst_ip\": \"7.7.7.7/32\", \"proto_type\": \"ip\", \"policy_id\": \"pol2\"}")
+        result_step4 = ret
+        utilities.assert_equals(expect=main.TRUE,actual=result_step4,
+                                onpass="Policy creation check PASS",
+                                onfail="Policy creation check FAIL")
+                                     
+        main.step("waiting 5 secs to push the tunnels and policies to the switches")
+        time.sleep(5)
+        main.step("verifying connectivity between hosts after tunnel policy creation")
+        p1 = main.Mininet.pingHost(SRC="h2",TARGET="h1")
+        ret_group_stats = main.FALSE
+        for entry in switch_groups:
+            if entry.has_key("SW"):
+                #print "entry[SW]: ",entry['SW']
+                after_stats = main.ONOS.get_switch_group_stats(onosRESTIP="127.0.0.1",
+                                      onosRESTPort=str(8080),
+                                      dpid=entry['SW'])
+                #print "after_stats: ",after_stats
+                if after_stats != main.FALSE:
+                    before_pkt_count = 0
+                    after_pkt_count = 0
+                    for group in entry['GROUPS']:
+                        for beforeStat in before_stats:
+                            if beforeStat['groupId'] == group:
+                                before_pkt_count = beforeStat['packetCount']
+                                break
+                        for afterStat in after_stats:
+                            if afterStat['groupId'] == group:
+                                after_pkt_count = afterStat['packetCount']
+                                break
+                    if (((after_pkt_count-before_pkt_count) > 0) and
+                        ((after_pkt_count-before_pkt_count) < 3)):
+                        ret_group_stats = main.TRUE
+                    else:
+                        ret_group_stats = main.FALSE
+                        break;
+
+        result_step5 = p1 and ret_group_stats
+        utilities.assert_equals(expect=main.TRUE,actual=result_step5,
+                                onpass="Connectivity check on tunnel policy PASS",
+                                onfail="Connectivity check on tunnel policy FAIL")
+
+        result_phase1 = result_step1 and result_step2 and result_step3 and result_step4 and result_step5
+
+        main.step("Verifying delete policy functionality")
+        ret = main.ONOS.delete_policy(onosRESTIP="127.0.0.1",
+                                      onosRESTPort=str(8080),
+                                      policyURL="/wm/onos/segmentrouting/policy",
+                                      params="{\"policy_id\": \"pol2\"}")
+        result_step6 = ret
+        utilities.assert_equals(expect=main.TRUE,actual=result_step5,
+                                onpass="Policy deletion check PASS",
+                                onfail="Policy deletion check FAIL")
+
+        main.step("Verifying delete tunnel functionality")
+        ret = main.ONOS.delete_tunnel(onosRESTIP="127.0.0.1",
+                                      onosRESTPort=str(8080),
+                                      tunnelURL="wm/onos/segmentrouting/tunnel",
+                                      params="{\"tunnel_id\":\"t2\"}")
+        result_step7 = ret
+        
+        utilities.assert_equals(expect=main.TRUE,actual=result_step7,
+                                onpass="Tunnel deletion check PASS",
+                                onfail="Tunnel deletion check FAIL")
+
+        main.step("waiting 5 secs for tunnel and policy remove to happen")
+        time.sleep(5)
+        main.step("verifying connectivity between hosts after tunnel policy deletion")
+        p1 = main.Mininet.pingHost(SRC="h2",TARGET="h1")
+        result_step8 = p1
+        utilities.assert_equals(expect=main.TRUE,actual=result_step8,
+                                onpass="Connectivity check after tunnel policy deletion PASS",
+                                onfail="Connectivity check after tunnel policy deletion FAIL")
+
+        result = result_phase1 and result_step6 and result_step7 and result_step8
+        utilities.assert_equals(expect=main.TRUE,actual=result,
+                                onpass="Tunnel Policy handling check PASS",
+                                onfail="Tunnel Policy handling check FAIL")
+
+        #cleanup mininet
+        main.Mininet.disconnect()
+
+
 #**********************************************************************************************************************************************************************************************
 # Restarts the controller in a linear 3-node topology
 
